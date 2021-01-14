61a1f1b8a7e458c3e2c6c0275d631a9834f3538b
Robert Knight <robertknight@gmail.com>
2021-01-14 08:46:43 +0000

    Add minified and ESM packages
    Add a minified version of the package to `dist/` and update the `main` field to
    point to the minified ESM version. This will allow convenient direct imports from CDNs etc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
   input: "build/index.js",
-  output: {
-    file: "dist/ureact.js",
-  },
+  output: [
+    { file: "dist/ureact.js", format: "cjs" },
+    { file: "dist/ureact.min.js", format: "cjs", plugins: [terser()] },
+    { file: "dist/ureact.esm.js", format: "esm", plugins: [terser()] },
+  ],
 };
